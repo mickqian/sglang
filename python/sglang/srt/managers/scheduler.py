260911979db40f955193f8f8c06565e366722d1f
Mick <mickjagger19@icloud.com>
2025-05-02 09:22:31 +0800

    fix review comments

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -2092,7 +2092,6 @@ class Scheduler(
         if self.profiler_activities is None:
             return
 
-        print(f"{self.profiler_activities=}")
         logger.info("Stop profiling...")
         if self.torch_profiler is not None:
             self.torch_profiler.stop()
